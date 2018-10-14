79e855359b25963ae2222aecc2d1c7b282f03f0b
Jimmy Huang <Jimmy@dhcp020-204-168.wireless.buffalo.edu>
2018-10-14 19:00:09 -0400

    The simple skeleton of an online shop created with the guidance of Agile Development with Rails book

---
+++ online_shop/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+Product.create!(title: 'One Mobile App', description: %{The One Mobile Application'},
+                image_url: '1app.jpg', price: 27.00)
